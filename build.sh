258265f652165b675e2bcbdf436e104fac78bc55
Joseph Richard <joseph.richard@redhat.com>
2021-02-23 23:09:06 -0500

    Switch to 0-9

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-for i in `seq 10`
+for i in `seq 0 9`
 do
     podman build --no-cache . -t quay.io/josephdrichard/workload_stress:image-${i}
     docker push quay.io/josephdrichard/workload_stress:image-${i}
